a6799fa2e8665e40a88c1a32a3ff5785a6920d2d
Geevfted <jesseadaramoye19@gmail.com>
2026-07-11 15:08:17 +0100

    Add Github Actions CI Pipeline

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "aws" {
-  access_key                  = "test"
-  secret_key                  = "test"
-  region                      = "us-east-1"
+  access_key = "test"
+  secret_key = "test"
+  region     = "us-east-1"
 
-  s3_use_path_style           = true
+  s3_use_path_style = true
 
   skip_credentials_validation = true
   skip_metadata_api_check     = true
